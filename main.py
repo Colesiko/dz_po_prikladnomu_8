956e1c95dd350ad3f95314abf5afbe5b421da503
Colesiko <49991441+Colesiko@users.noreply.github.com>
2022-12-21 22:17:05 +0500

    добавление проверки вводимых символов

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ import logging
 import random
 logging.basicConfig(filename='draw.log', level=logging.INFO)
 logging.info("Programm started")
-n = int(input("Введите размер мешочка: "))
+n = input("Введите размер мешочка: ")
+bukv = n.isdigit()
+if bukv == False:  # проверка на буквы
+    while bukv == False:
+        n = input("Введите число большее 0(цифрами): ")
+        bukv = str.isnumeric(n)
+n = int(n)
+if n <= 0:
+    while n <= 0:
+        n = input("Введите число большее 0(цифрами): ")
+        bukv = n.isdigit()
+        if bukv == False:  # проверка на буквы
+            while bukv == False:
+                n = input("Введите число большее 0(цифрами): ")
+                bukv = str.isnumeric(n)
+        n = int(n)
+logging.info(f'razmer meshochka {n}')
 # создаем массив чисел от 1 до n
 meshok = list(range(1, n + 1))
 
